2e1e508a7fea0cd034f271b6a229497858a4f3fc
Daniel Schmitz <daniel.schmitz@igp.uu.se>
2021-03-11 18:52:36 +0100

    Fixed styling

--- R/plotting.R
+++ R/plotting.R
@@ -13,12 +13,17 @@ create_plot <- function(table, col_headers = TRUE, row_headers = TRUE) {
   stopifnot(is.SimplePlotTable(table))
   plot <- ggplot2::ggplot() +
     create_geom(table$data) +
-    ggplot2::theme_void()
+    theme_void()  +
+    ggplot2::scale_x_continuous(position="top",
+                              labels=table$cols,
+                              breaks=1:length(table$cols)) +
+    ggplot2::scale_y_continuous(labels=rev(table$rows),
+                                breaks=1:length(table$rows))
   if (col_headers) {
-    plot <<- plot + scale_x_discrete(position="top", labels=table$cols)
+    plot <- plot + ggplot2::theme(axis.text.x = ggplot2::element_text(face="bold"))
   }
   if (row_headers) {
-    plot <<- plot + scale_y_discrete(labels = rev(table$rows))
+    plot <- plot + ggplot2::theme(axis.text.y = ggplot2::element_text(face="bold"))
   }
   plot
 }
