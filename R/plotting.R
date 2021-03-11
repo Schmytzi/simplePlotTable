7bc7bf485a766408f65fd5c868a7397a6d7d05df
Daniel Schmitz <daniel.schmitz@igp.uu.se>
2021-03-11 17:42:57 +0100

    Added support for arbitrary styles

--- R/plotting.R
+++ R/plotting.R
@@ -1,14 +1,9 @@
 create_geom <- function(long_data){
+  mappings <- list(x="col", y="rev_row", label = "text")
+  style_attributes <- colnames(long_data)[-(1:3)]
+  mappings[style_attributes] <- style_attributes
   ggplot2::geom_text(
     data=long_data,
-    mapping=aes(
-      x = x,
-      y = y,
-      label = text,
-      fontface = fontface,
-      family = family,
-      size = size,
-      hjust = hjust,
-      vjust = vjust)
+    mapping=do.call(aes_string, mappings)
   )
 }
