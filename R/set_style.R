83bade9adc5335952acefad706aa84a26a96d69e
Daniel Schmitz <daniel.schmitz@igp.uu.se>
2021-03-11 18:51:53 +0100

    Fixed wrong variable reference

--- R/set_style.R
+++ R/set_style.R
@@ -1,10 +1,10 @@
 
-set_style <- function(table, rows=1:nrow(table), cols=1:ncol(table), ...) {
+set_style <- function(table, rows=1:nrow(table$data), cols=1:ncol(table$data), ...) {
   stopifnot(is.SimplePlotTable(table))
   cell_indexes <- which(table$data$row %in% rows & table$data$col %in% cols)
   styles <- list(...)
-  for (attribute in names(list)){
-    table$data[cell_indexes, attribute] <- list$attribute
+  for (attribute in names(styles)){
+    table$data[cell_indexes, attribute] <- styles[[attribute]]
   }
   table
 }
