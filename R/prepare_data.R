7ee8f9d3b6c9a1b932cab9804d3afee839aef671
Daniel Schmitz <daniel.schmitz@igp.uu.se>
2021-03-11 17:33:37 +0100

    Removed default styles

--- R/prepare_data.R
+++ R/prepare_data.R
@@ -2,15 +2,10 @@ prepare_data <- function(data) {
   stopifnot(is.data.frame(data))
   rows <- rownames(data)
   cols <- colnames(data)
-  rownames(data) <- nrow(data):1
+  rownames(data) <- 1:nrow(data)
   colnames(data) <- 1:ncol(data)
-  data$y <- 1:nrow(data)
-  long_data <- tidyr::pivot_longer(data, !y, names_to="x", values_to="text")
-  long_data$fontface <- "plain"
-  long_data$family <- "sans"
-  long_data$size <- 12 / ggplot2::.pt
-  long_data$hjust <- .5
-  long_data$vjust <- .5
+  data$row <- 1:nrow(data)
+  long_data <- tidyr::pivot_longer(data, !y, names_to="col", values_to="text")
   result <- list(data=long_data, cols=cols, rows=rows)
   class(result) <- "SimplePlotTable"
 }
