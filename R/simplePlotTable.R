263b598967ff3222b1faf171d8b90b45923f8924
Daniel Schmitz <daniel.schmitz@igp.uu.se>
2021-03-11 18:08:46 +0100

    Fixed wrong function call

--- R/simplePlotTable.R
+++ R/simplePlotTable.R
@@ -1,11 +1,14 @@
 
 create_table <- function(data, col_names = colnames(data), row_names = rownames(data)){
   stopifnot(is.data.frame(data))
-  colnames(data) <- col_names
-  rownames(data) <- row_names
-  table <- prepare_data(data)
-}
+  stopifnot(is.null(col_names) || length(col_names) == ncol(data))
+  stopifnot(is.null(row_names) || length(row_names) == nrow(data))
 
+  table <- prepare_data(data)
+  table$cols <- col_names
+  table$rows <- row_names
+  table
+}
 
 is.SimplePlotTable <- function(table) {
   "SimplePlotTable" %in% class(table)
