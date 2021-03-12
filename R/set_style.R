e84ad3bd59371bc4f50a79ebf6ea435b8d860b50
Daniel Schmitz <daniel.schmitz@igp.uu.se>
2021-03-12 08:16:12 +0100

    Fixed wrong default arguments

--- R/set_style.R
+++ R/set_style.R
@@ -1,5 +1,5 @@
 
-set_style <- function(table, rows=1:nrow(table$data), cols=1:ncol(table$data), ...) {
+set_style <- function(table, rows=1:length(table$rows), cols=1:length(table$cols), ...) {
   stopifnot(is.SimplePlotTable(table))
   cell_indexes <- which(table$data$row %in% rows & table$data$col %in% cols)
   styles <- list(...)
